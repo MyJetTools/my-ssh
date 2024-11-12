143a10802dd2c05830d381e07a49bbd9df08b413
amigin <amigin@gmail.com>
2024-11-12 09:54:35 +0100

    Now we have SSH_SESSIONS_POOL - which can be used

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 mod error;
+use std::sync::Arc;
+
 pub use error::*;
 mod ssh_session;
 pub use ssh_session::*;
@@ -10,6 +12,7 @@ pub use ssh_session_inner::*;
 mod ssh_session_wrapper;
 pub use ssh_session_wrapper::*;
 
+#[deprecated(note = "Please use SshAsyncChannel instead")]
 pub type SshAsyncSession = async_ssh2_lite::AsyncSession<async_ssh2_lite::TokioTcpStream>;
 
 pub type SshAsyncChannel = async_ssh2_lite::AsyncChannel<async_ssh2_lite::TokioTcpStream>;
@@ -21,3 +24,7 @@ mod ssh_sessions_pool;
 pub use ssh_sessions_pool::*;
 mod over_ssh_connection_settings;
 pub use over_ssh_connection_settings::*;
+
+lazy_static::lazy_static! {
+    static ref SSH_SESSIONS_POOL: Arc<crate::SshSessionsPool> =  Arc::new(crate::SshSessionsPool::new());
+}
