a84484bcd3cf84bf4ba35901866fe7ec8c24a3ae
amigin <amigin@gmail.com>
2024-12-05 14:05:24 +0200

    Naming Fixes

--- src/over_ssh_connection_settings.rs
+++ src/over_ssh_connection_settings.rs
@@ -101,12 +101,12 @@ fn parse_ssh_string(src: &str) -> crate::SshCredentials {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct SshCredentialsSettingsModel {
+pub struct SshPrivateKeySettingsModel {
     pub cert_path: String,
     pub cert_pass_phrase: Option<String>,
 }
 
-impl SshCredentialsSettingsModel {
+impl SshPrivateKeySettingsModel {
     pub async fn load_cert(&self) -> String {
         let file = rust_extensions::file_utils::format_path(self.cert_path.as_str());
         let cert_content = tokio::fs::read_to_string(file.as_str()).await;
