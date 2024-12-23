cf73d11ac03e5b9a170f021d7360af5307230ea9
amigin <amigin@gmail.com>
2024-12-23 23:49:04 +0200

    Fixes

--- src/ssh_settings/ssh_security_credentials_resolver.rs
+++ src/ssh_settings/ssh_security_credentials_resolver.rs
@@ -1,3 +1,7 @@
+use std::sync::Arc;
+
+use crate::SshCredentials;
+
 #[derive(Debug, Clone)]
 pub struct SshPrivateKey {
     pub content: String,
@@ -8,4 +12,31 @@ pub struct SshPrivateKey {
 pub trait SshSecurityCredentialsResolver {
     async fn resolve_ssh_private_key(&self, ssh_line: &str) -> Option<SshPrivateKey>;
     async fn resolve_ssh_password(&self, ssh_line: &str) -> Option<String>;
+
+    async fn update_credentials(
+        &self,
+        ssh_line: &str,
+        ssh_credentials: Arc<SshCredentials>,
+    ) -> Arc<SshCredentials> {
+        if let Some(private_key) = self.resolve_ssh_private_key(ssh_line).await {
+            return Arc::new(SshCredentials::PrivateKey {
+                ssh_remote_host: ssh_credentials.get_host_port().0.to_string(),
+                ssh_remote_port: ssh_credentials.get_host_port().1,
+                ssh_user_name: ssh_credentials.get_user_name().to_string(),
+                private_key: private_key.content,
+                passphrase: private_key.pass_phrase,
+            });
+        }
+
+        if let Some(password) = self.resolve_ssh_password(ssh_line).await {
+            return Arc::new(SshCredentials::UserNameAndPassword {
+                ssh_remote_host: ssh_credentials.get_host_port().0.to_string(),
+                ssh_remote_port: ssh_credentials.get_host_port().1,
+                ssh_user_name: ssh_credentials.get_user_name().to_string(),
+                password,
+            });
+        }
+
+        ssh_credentials
+    }
 }
